184b512aa3771a8e307cfcadb8841568caf57ea8
STOKYS <stocekdavid.net@gmail.com>
2022-06-06 00:02:31 +0200

    Update v22.06.06:00 - [Data view]

--- flight/urls.py
+++ flight/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('person/<int:person_id>', views.person, name='person'),
+    path('flights/', views.flights, name='flights'),
     path('flight/<int:flight_id>', views.flight, name='flight'),
+    path('person/<int:person_id>', views.person, name='person'),
+    # path('flight/<int:flight_id>', views.flight, name='flight'),
 ]
--- flight/views.py
+++ flight/views.py
@@ -15,26 +15,35 @@ def index(request):
     return render(request, 'index.html', context=context)
 
 
-def person(request, person_id):
-    context = {
-        'person_filter': Person.objects.filter(id=person_id).values_list(flat=True).distinct(),
-        'person_get': Person.objects.get(pk=person_id)
-    }
-    return render(request, 'page/detail.html', context=context)
-
-
-# def flight(request, flight_id):
+# def person(request, person_id):
 #     context = {
-#         'person_filter': Flight.objects.filter(id=flight_id).values_list(flat=True).distinct(),
-#         'person_get': Flight.objects.get(pk=flight_id)
+#         'person_filter': Person.objects.filter(id=person_id).values_list(flat=True).distinct(),
+#         'person_get': Person.objects.get(pk=person_id)
 #     }
 #     return render(request, 'page/detail.html', context=context)
 
 
+def flights(request):
+    context = {
+        'flights': Flight.objects.all(),
+    }
+    return render(request, 'page/list.html', context=context)
+
 
 def flight(request, flight_id):
     context = {
-        'flight': Flight.objects.get(pk=flight_id),
+        'flight_filter': Flight.objects.filter(id=flight_id).values_list(flat=True).distinct().first(),
+        'flight_get': Flight.objects.get(pk=flight_id),
+        'flight_staff': Staff.objects.filter(flight=flight_id),
+        'flight_passenger': Passenger.objects.filter(flight=flight_id),
     }
-    return render(request, 'flight/flight.html', context=context)
+    return render(request, 'flight/detail.html', context=context)
 
+
+def person(request, person_id):
+    context = {
+        'person': Person.objects.get(pk=person_id),
+        'passengers': Passenger.objects.filter(person=person_id),
+        'staffs': Staff.objects.filter(person=person_id)
+    }
+    return render(request, 'person/person.html', context=context)
